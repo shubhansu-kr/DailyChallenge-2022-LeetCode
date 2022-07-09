845a6ef00917da6a18790c4d418c033a1f5fa9fa
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-09 14:07:34 +0530

    SOlution 4: MonoQueue DP

--- 07_July_2022/08_JumpGameVI.cpp
+++ 07_July_2022/08_JumpGameVI.cpp
@@ -12,6 +12,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution4
+{
+    // Discussion Solution 
+public:
+    int maxResult(vector<int> &nums, int k)
+    {
+        long curr = 0;
+        deque<int> dq; // Monoqueue
+
+        // Start answering the best possible results for index `i` from the back
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+            curr = nums[i] + (dq.empty() ? 0 : nums[dq.front()]); // Get current best, the maximum result will be the first element in the monoqueue.
+
+            // We remove all the smaller results greedily as they wont get used anyways (This helps the monoqueue to remain sorted)
+            while (!dq.empty() && curr > nums[dq.back()])
+                dq.pop_back();
+            dq.push_back(i); // Insert current index into the monoqueue
+
+            // Erase all the indices in deque that are greater than or equal to i+k.
+            if (dq.front() >= i + k)
+                dq.pop_front();
+            nums[i] = curr; // Use input array as auxillary array to store the best results.
+        }
+        return curr; // `curr` would have the best or the maxium result to reach end from index 0.
+    }
+};
+
 class Solution3
 {
     // Tabulation
@@ -27,14 +55,15 @@ public:
             int subMax = INT_MIN;
             for (int i = 1; i <= k; ++i)
             {
-                if (j - i < 0) break;
-                    
+                if (j - i < 0)
+                    break;
+
                 int sum = nums[j] + dp[j - i];
                 subMax = max(sum, subMax);
             }
             dp[j] = subMax;
         }
-        return dp[n-1];
+        return dp[n - 1];
     }
 };
 
