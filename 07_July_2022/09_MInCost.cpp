ad1107aa287e196d64e5b52e0e60fd8584348020
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-10 14:28:41 +0530

    Solution 2: Tabulation

--- 07_July_2022/09_MInCost.cpp
+++ 07_July_2022/09_MInCost.cpp
@@ -9,6 +9,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution2
+{
+    // Recursion: Memoization
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        int n = cost.size();
+        vector<int> dp(n);
+        dp[0] = cost[0], dp[1] = min(cost[0], cost[1]);
+        for (int i = 2; i < n; ++i) dp[i] = cost[i] + min(dp[i-1], dp[i-2]); 
+        return min (dp[n-2], dp[n-1]);
+    }
+};
+
 class Solution1
 {
     // Recursion: Memoization
