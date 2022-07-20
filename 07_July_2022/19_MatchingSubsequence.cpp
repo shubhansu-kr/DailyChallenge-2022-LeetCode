8b493306302cd53810487b8f228808d199b14346
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-20 11:55:50 +0530

    TLE

---
+++ 07_July_2022/19_MatchingSubsequence.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/number-of-matching-subsequences/
+
+// Given a string s and an array of strings words, return the number 
+// of words[i] that is a subsequence of s.
+// A subsequence of a string is a new string generated from the original 
+// string with some characters (can be none) deleted without changing 
+// the relative order of the remaining characters.
+// For example, "ace" is a subsequence of "abcde"
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+    // BruteForce: TLE
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        unordered_set<string> track;
+        string temp = "";
+
+        subSeq(s, track, temp);
+
+        int ans = 0;
+        for(auto &it: words) {if (track.count(it)) ans++;}
+        return ans;
+    }
+    void subSeq(string &s, unordered_set<string> &track, string &temp, int i = 0) {
+        if (i == s.size()) {
+            track.insert(temp);
+            return ;
+        }
+
+        temp += s[i] ;
+        subSeq(s, track, temp, i+1);
+        temp.pop_back();
+        subSeq(s, track, temp, i+1);
+    }
+};
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
