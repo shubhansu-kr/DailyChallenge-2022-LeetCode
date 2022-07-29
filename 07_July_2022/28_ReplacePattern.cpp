1c676a8d4598f64475212087dc785903f843b66f
shubhansu-kr <shubhansu2021@gmail.com>
2022-07-29 18:18:55 +0530

    Find patterns

--- 07_July_2022/28_ReplacePattern.cpp
+++ 07_July_2022/28_ReplacePattern.cpp
@@ -1,52 +1,108 @@
 // https://leetcode.com/problems/find-and-replace-pattern/
 
-// Given a list of strings words and a string pattern, return a list 
+// Given a list of strings words and a string pattern, return a list
 // of words[i] that match pattern. You may return the answer in any order.
-// A word matches the pattern if there exists a permutation of letters 
-// p so that after replacing every letter x in the pattern with p(x), 
+// A word matches the pattern if there exists a permutation of letters
+// p so that after replacing every letter x in the pattern with p(x),
 // we get the desired word.
-// Recall that a permutation of letters is a bijection from letters 
-// to letters: every letter maps to another letter, and no two letters 
+// Recall that a permutation of letters is a bijection from letters
+// to letters: every letter maps to another letter, and no two letters
 // map to the same letter.
 
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-class Solution {
+class Solution
+{
+    // Using maps and sets 
 public:
-    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
-        if (pattern.length() == 1) return words;
-        vector<string> ans;
-        for(string &s: words) {
-            // check for each word and validate the pattern 
-            unordered_map<char, char> m;
-            unordered_set<char> us; 
-            int flag = 1 ;
-            for (int i = 0; i < s.length(); ++i)
+    vector<string> findAndReplacePattern(vector<string> &words, string pattern)
+    {
+        vector<string> res;
+        for (int i = 0; i < words.size(); i++)
+        {
+            unordered_map<char, char> permute;
+            unordered_set<char> track;
+            int flag = 1;
+            for (int j = 0; j < words[i].size(); j++)
             {
-                if(us.count(s[i])) {
-                    if (m[s[i]] == pattern[i]) {
-                        continue;
-                    }
-                    else {
-                        flag = 0 ;
+                if (permute.count(pattern[j]))
+                {
+                    if (permute[pattern[j]] != words[i][j])
+                    {
+                        flag = 0;
                         break;
                     }
                 }
-                else {
+                else
+                {
+                    if (track.count(words[i][j]))
+                    {
+                        flag = 0;
+                        break;
+                    }
+                    else
+                    {
+                        track.insert(words[i][j]);
+                        permute[pattern[j]] = words[i][j];
+                    }
+                }
+            }
+            if (flag)
+            {
+                res.push_back(words[i]);
+            }
+        }
+        return res;
+    }
+};
+
+class Solution
+{
+    // Incomplete solution
+public:
+    vector<string> findAndReplacePattern(vector<string> &words, string pattern)
+    {
+        if (pattern.length() == 1)
+            return words;
+        vector<string> ans;
+        for (string &s : words)
+        {
+            // check for each word and validate the pattern
+            unordered_map<char, char> m;
+            unordered_set<char> us;
+            int flag = 1;
+            for (int i = 0; i < s.length(); ++i)
+            {
+                if (us.count(s[i]))
+                {
+                    if (m[s[i]] == pattern[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+                else
+                {
                     us.insert(s[i]);
                     m[s[i]] = pattern[i];
                 }
             }
-            if (flag) {
+            if (flag)
+            {
                 ans.emplace_back(s);
             }
         }
-        return ans; 
+        return ans;
     }
 };
 
-int main () {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     return 0;
