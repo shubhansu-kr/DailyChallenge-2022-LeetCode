c5f1ed4e8670aa4d93bebf95580686cccabf5223
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-01 16:28:42 +0530

    Solution3: Space Optimised

--- 08_August_2022/00_UniquePath.cpp
+++ 08_August_2022/00_UniquePath.cpp
@@ -15,6 +15,19 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution3 {
+    // Tabulation: Space Optimised 
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n, 1);
+        
+        for (int i = 1; i < m; ++i)
+            for (int j = 1; j < n; ++j)
+                dp[j] = dp[j] + dp[j-1];  
+        return dp[n-1];
+    }
+};
+
 class Solution2 {
     // Tabulation 
 public:
