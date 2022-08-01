d6bdb245c344ad6d53d5b16206af1eb4c8ab4d1b
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-01 16:11:10 +0530

    Solution1: Memoization

--- 08_August_2022/00_UniquePath.cpp
+++ 08_August_2022/00_UniquePath.cpp
@@ -16,6 +16,23 @@
 using namespace std ;
 
 class Solution {
+    // Recursion: Memoization 
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        
+        return findPath (m-1, n-1, dp);
+    }
+
+    int findPath (int i, int j, vector<vector<int>> &dp) {
+        if (i < 0 || j < 0) return 0;
+        if (i == 0 && j == 0) return 1;
+        if (dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = findPath(i-1, j, dp) + findPath(i, j-1, dp);
+    }
+};
+
+class Solution {
     // BruteForce: Recursion 
 public:
     int uniquePaths(int m, int n) {
