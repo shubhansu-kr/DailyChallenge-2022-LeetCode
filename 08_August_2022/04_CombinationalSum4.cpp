f8aa3c94b1b67a8a3f9ad2d145e8024448299127
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-05 21:57:45 +0530

    Combinational Sum

--- 08_August_2022/04_CombinationalSum4.cpp
+++ 08_August_2022/04_CombinationalSum4.cpp
@@ -4,6 +4,18 @@
 using namespace std ;
 
 class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target+1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++)
+            for (int num : nums)
+                if (num <= i) dp[i] += dp[i-num];
+        return dp[target];
+    }
+};
+
+class Solution {
     // Wrong Answer 
 public:
     int combinationSum4(vector<int>& nums, int target) {
