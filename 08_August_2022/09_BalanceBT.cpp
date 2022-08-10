dbb04b29df9cca91880e75703d4c44ae404e98e2
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-10 20:43:38 +0530

    Balance BST

--- 08_August_2022/09_BalanceBT.cpp
+++ 08_August_2022/09_BalanceBT.cpp
@@ -1,9 +1,10 @@
 // https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-struct TreeNode {
+struct TreeNode
+{
     int val;
     TreeNode *left;
     TreeNode *right;
@@ -12,26 +13,55 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
-    // Wrong Solution 
-public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        int n = nums.size();
-        return createTree(nums, n/2);
+class Solution
+{
+    TreeNode *sortedArrayToBST(vector<int> &nums, int start, int end)
+    {
+        if (end <= start)return NULL;
+        int midIdx = (end + start) / 2;
+        TreeNode *root = new TreeNode(nums[midIdx]);
+        root->left = sortedArrayToBST(nums, start, midIdx);
+        root->right = sortedArrayToBST(nums, midIdx + 1, end);
+        return root;
     }
 
-    TreeNode* createTree(vector<int> &nums, int i) {
-        if (i == -1 || i == nums.size()) return nullptr;
-        
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        return sortedArrayToBST(nums, 0, nums.size());
+    }
+};
+class Solution
+{
+    // Wrong Solution
+public:
+    TreeNode *sortedArrayToBST(vector<int> &nums)
+    {
+        int n = nums.size();
+        return createTree(nums, n / 2);
+    }
+
+    TreeNode *createTree(vector<int> &nums, int i)
+    {
+        if (i == -1 || i == nums.size())
+            return nullptr;
+
         TreeNode *p = new TreeNode(nums[i]);
-        if (i <= nums.size()/2){p->left = createTree(nums, i-1);}
-        if (i >= nums.size()/2) {p->right = createTree(nums, i+1);}
+        if (i <= nums.size() / 2)
+        {
+            p->left = createTree(nums, i - 1);
+        }
+        if (i >= nums.size() / 2)
+        {
+            p->right = createTree(nums, i + 1);
+        }
 
         return p;
     }
 };
 
-int main () {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     return 0;
