3503a5a18ec5c1889a20dff703ea7a15e8e21a1d
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-11 20:03:23 +0530

    Check if BST is valid

---
+++ 08_August_2022/10_ValidBST.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        TreeNode* prev = NULL;
+        return validate(root, prev);
+    }
+    bool validate(TreeNode* node, TreeNode* &prev) {
+        if (node == NULL) return true;
+        if (!validate(node->left, prev)) return false;
+        if (prev != NULL && prev->val >= node->val) return false;
+        prev = node;
+        return validate(node->right, prev);
+    }
+};
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
