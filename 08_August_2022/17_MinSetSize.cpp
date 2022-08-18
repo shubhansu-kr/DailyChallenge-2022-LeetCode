b0e585900dac69af737a2150a4c946bb63e1dd97
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-18 20:49:17 +0530

    Solution3: Frequency+CountSort

--- 08_August_2022/17_MinSetSize.cpp
+++ 08_August_2022/17_MinSetSize.cpp
@@ -8,6 +8,26 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution3 {
+    // Clean and Concise: count sort
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int, int> cnt;
+        for (int x : arr) ++cnt[x];
+        
+        vector<int> frequencies;
+        for (auto [_, freq] : cnt) frequencies.push_back(freq);
+        sort(frequencies.begin(), frequencies.end());
+        
+        int ans = 0, removed = 0, half = arr.size() / 2, i = frequencies.size() - 1;
+        while (removed < half) {
+            ans += 1;
+            removed += frequencies[i--];
+        }
+        return ans;
+    }
+};
+
 class Solution2 {
     // Using Maps: Tabulation : TLE 
 public:
