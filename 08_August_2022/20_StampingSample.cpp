be1c3d0ebefd1a54477ad5aa8c950924251ac497
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-21 20:51:39 +0530

    Stamping Sequence

---
+++ 08_August_2022/20_StampingSample.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/stamping-the-sequence/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    // Discussion solution: Way too extreme for me to solve 
+public:
+    vector<int> movesToStamp(string stamp, string target)
+    {
+        vector<int> res;
+        auto total_stamp = 0, turn_stamp = -1;
+        while (turn_stamp)
+        {
+            turn_stamp = 0;
+            for (int sz = stamp.size(); sz > 0; --sz)
+                for (auto i = 0; i <= stamp.size() - sz; ++i)
+                {
+                    auto new_stamp = string(i, '*') + stamp.substr(i, sz) + string(stamp.size() - sz - i, '*');
+                    auto pos = target.find(new_stamp);
+                    while (pos != string::npos)
+                    {
+                        res.push_back(pos);
+                        turn_stamp += sz;
+                        fill(begin(target) + pos, begin(target) + pos + stamp.size(), '*');
+                        pos = target.find(new_stamp);
+                    }
+                }
+            total_stamp += turn_stamp;
+        }
+        reverse(begin(res), end(res));
+        return total_stamp == target.size() ? res : vector<int>();
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
