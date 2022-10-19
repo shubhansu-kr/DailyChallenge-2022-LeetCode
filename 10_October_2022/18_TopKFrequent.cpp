dade8d2ac135156898a4b097895c564caacaa254
Shubhansu kumar <shubhansu2021@gmail.com>
2022-10-19 23:38:59 +0530

    FInd the list of k most frequent elements
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 10_October_2022/18_TopKFrequent.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/top-k-frequent-words/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+// Discussion Solution 
+struct Compare {
+    bool operator() (pair<int, string> a, pair<int, string> b) {
+        if(a.first == b.first) return a.second > b.second;
+        else return a.first < b.first;
+    }
+};
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> m;
+        for(int i=0; i<words.size(); i++) m[words[i]]++;
+        
+        priority_queue<pair<int, string>, vector<pair<int, string>>, Compare> q;
+        for(auto p : m) q.push({p.second, p.first});
+        
+        vector<string> ans;
+        while(k--) {
+            ans.push_back(q.top().second);
+            q.pop();
+        }
+        
+        return ans;
+    }
+};
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
