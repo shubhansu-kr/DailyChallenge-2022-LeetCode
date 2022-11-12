eb6cece6e74bdd008704e7e4bd791780a078cf0d
Shubhansu kumar <shubhansu2021@gmail.com>
2022-11-12 20:39:58 +0530

    Find the median
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 11_November_2022/11_DataStream.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/contest/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class MedianFinder {
+    priority_queue<long> small, large;
+public:
+    void addNum(int num) {
+        small.push(num);
+        large.push(-small.top());
+        small.pop();
+        if (small.size() < large.size()) {
+            small.push(-large.top());
+            large.pop();
+        }
+    }
+    double findMedian() {
+        return small.size() > large.size()
+               ? small.top()
+               : (small.top() - large.top()) / 2.0;
+    }
+};
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
