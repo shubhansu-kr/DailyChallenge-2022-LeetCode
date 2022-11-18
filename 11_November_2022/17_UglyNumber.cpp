71710b6b95577db5910db962f7c8dee9e22dfe12
Shubhansu kumar <shubhansu2021@gmail.com>
2022-11-18 20:36:25 +0530

    FInd ugly number
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ 11_November_2022/17_UglyNumber.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/ugly-number/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n < 1) return false;
+        while(n % 2 == 0) n /= 2;
+        while(n % 3 == 0) n /= 2;
+        while(n % 5 == 0) n /= 5;
+
+        return n == 1;
+    }
+};
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
