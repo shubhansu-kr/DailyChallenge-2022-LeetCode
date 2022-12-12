712e2313537ed0f77129ff83ebe07b92433ac4e1
Shubhansu kumar <shubhansu2021@gmail.com>
2022-12-12 21:23:08 +0530

    SOlution1: Memoization
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- 12_December_2022/11_ClimbStairs.cpp
+++ 12_December_2022/11_ClimbStairs.cpp
@@ -3,6 +3,20 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
+class Solution1 {
+    // Memoization
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return countWays(n, dp);
+    }
+    int countWays(int n, vector<int> &dp) {
+        if (n <= 0) return n == 0;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = countWays(n-1, dp) + countWays(n-2, dp);
+    }
+};
+
 class Solution {
     // BruteForce : Recursion 
 public:
